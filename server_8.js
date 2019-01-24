5bd20a0b386df92bace0786e749afc7cf21eda64
Kamil <kamil.lemisz@gmail.com>
2019-01-24 23:37:24 +0100

    simple socket communication between two browsers

---
+++ server_8.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+//alternativly use express
+function handleHTTP (req,res) {
+	if (req.method === "GET") {
+		if (/^\/\d+(?=$|[\/?#])/.test(req.url)) {
+			req.addListener("end", function() {
+				req.url = req.url.replace(/^\/(\d+).*$/,"/$1.html"); //replace ex. 6 to 6.html to serve static html
+				static_files.serve(req,res);
+			});
+			req.resume();
+		}
+	  	else {
+			res.writeHead(403);
+			res.end("Forbbiden");
+		}
+	}
+	else {
+		res.writeHead(403);
+		res.end("Forbbiden");
+	}
+}
+
+function handleIO(socket) {
+	function disconnect() {
+		clearInterval(intv);
+		console.log("client disconnected");
+	}
+
+	console.log("client connected");
+
+	socket.on("disconnect", disconnect);
+
+	
+	socket.on("typeit", function(msg) {
+		//broadcast to everebody except this socket
+		socket.broadcast.emit("messages",msg);
+		//message to everybody including the current one
+		//io.broadcast
+	});
+
+	var intv = setInterval(function(){
+		socket.emit("hello", Math.random());
+	},1000);
+}
+
+var hostname = '127.0.0.1';
+var port = 3000;
+
+var http = require("http");
+var server = http.createServer(handleHTTP).listen(port,hostname);
+
+var ASQ =  require("asynquence")
+	
+var node_static = require("node-static");
+
+var static_files =  new node_static.Server(__dirname + "/public/html");
+
+var io = require("socket.io").listen(server);
+
+io.on("connection", handleIO);
+
+//configure socket.io
+io.configure(function() {
+	io.enable("browser client minification");
+	io.enable("browser client etag"); //apply etag caching
+	io.set("log level", 1); //reduce logging
+	io.set("transports", [
+		"websocket",
+		"xhr-polling",
+		"jsonp-polling"
+	]);
+})
