e38c3e80637144b2f4f73c885f9a0c1933a55750
klemis <kamil.lemisz@gmail.com>
2019-03-23 17:16:38 +0100

    static files path

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-//   .use(express.static(path.join(__dirname, 'public')))
+  .use("/static", express.static(path.join(__dirname, 'public')))
 //   .set('views', path.join(__dirname, 'public/html'))
 //   .set('view engine', 'ejs')
   .get('/', (req, res) => res.sendFile(__dirname + '/public/html/14.html'))
